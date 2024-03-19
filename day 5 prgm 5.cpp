7b27b797834b1779929f725c44cf69b9c3b5f30c
192211698 <124115464+192211698@users.noreply.github.com>
2024-03-19 13:23:08 +0530

    Update day 5 prgm 5.cpp

--- day 5 prgm 5.cpp
+++ day 5 prgm 5.cpp
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Function to calculate total head movement
 int calculateHeadMovement(int queue[], int head, int size) {
 int totalMovement = 0;
 
-// Traverse the queue and calculate head movement
 for (int i = 0; i < size; i++) {
 totalMovement += abs(queue[i] - head);
 head = queue[i];
@@ -16,18 +14,18 @@ return totalMovement;
 }
 
 int main() {
-int n; // Number of disk requests
+int n; 
 printf("Enter the number of disk requests: ");
 scanf("%d", &n);
 
-int requestQueue[n]; // Disk request queue
+int requestQueue[n]; 
 
 printf("Enter the disk request queue:\n");
 for (int i = 0; i < n; i++) {
 scanf("%d", &requestQueue[i]);
 }
 
-int initialHead; // Initial position of the head
+int initialHead; 
 printf("Enter the initial position of the head: ");
 scanf("%d", &initialHead);
 
